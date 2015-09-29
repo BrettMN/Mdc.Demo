f7d0fd11825a08a8aa59f81ba54da899d88705e0
BrettMN <brettmn@outlook.com>
2015-09-29 13:11:38 -0700

    update dataService

--- Mdc.Demo.BLL/Services/DataService.cs
+++ Mdc.Demo.BLL/Services/DataService.cs
@@ -9,47 +9,65 @@ namespace Mdc.Demo.BLL.Services
 {
     public class DataService : IDataService
     {
-        private const string _file = @"tasks.json";
         private static Dictionary<int, Models.Task> _tasks;
+        private Dictionary<int, Models.Task> Tasks
+        {
+            get
+            {
+                if (_tasks == null)
+                {
+                    _tasks = new Dictionary<int, Models.Task>();
+                    _tasks.Add(0, new Models.Task { Id = 0, Complete = true, Description = "Don't Panic" });
+                    _tasks.Add(1, new Models.Task { Id = 1, Complete = false, Description = "Give Presentaion" });
+                    _tasks.Add(2, new Models.Task { Id = 2, Complete = false, Description = "Relax" });
+                }
+                return _tasks;
+            }
+            set
+            {
+                _tasks = value;
+            }
+        }
+
 
         public DataService()
         {
-            if (_tasks == null)
-            {
-                _tasks = new Dictionary<int, Models.Task>();
-                _tasks.Add(0, new Models.Task { Id = 0, Complete = true, Description = "Don't Panic" });
-                _tasks.Add(1, new Models.Task { Id = 1, Complete = false, Description = "Give Presentaion" });
-                _tasks.Add(2, new Models.Task { Id = 2, Complete = false, Description = "Relax" });
-            }
+            //if (_tasks == null)
+            //{
+            //    _tasks = new Dictionary<int, Models.Task>();
+            //    _tasks.Add(0, new Models.Task { Id = 0, Complete = true, Description = "Don't Panic" });
+            //    _tasks.Add(1, new Models.Task { Id = 1, Complete = false, Description = "Give Presentaion" });
+            //    _tasks.Add(2, new Models.Task { Id = 2, Complete = false, Description = "Relax" });
+            //}
         }
 
         public Models.Task AddNewTask(Models.Task task)
         {
-            task.Id = _tasks.Count > 0 ? _tasks.Keys.Max() + 1 : 0;
-            _tasks.Add(task.Id, task);
+            task.Id = Tasks.Count > 0 ? Tasks.Keys.Max() + 1 : 0;
+            Tasks.Add(task.Id, task);
 
             return task;
         }
 
         public IEnumerable<Models.Task> GetAllTasks()
         {
-            return _tasks.Values;
+            return Tasks.Values;
         }
 
         public Models.Task UpdateTask(Models.Task task)
         {
-            _tasks[task.Id] = task;
+            Tasks[task.Id] = task;
             return task;
         }
 
         public void DeleteTask(int id)
         {
-            _tasks.Remove(id);
+            Tasks.Remove(id);
         }
 
         public Models.Task GetTask(int id)
         {
-            return _tasks[id];
+            return Tasks[id];
         }
     }
 }
